f93908d352bdfecd3978c940f6e8ab13dab09b66
litmus-star <ayanoquasar@gmail.com>
2024-03-15 13:52:23 +0900

    i18n (ja-JP)

---
+++ lumiutau/assets/js/language.js
@@ -0,0 +1,18 @@
+function redirectToLanguagePage() {
+    // 사용자의 브라우저 언어 설정을 가져옵니다.
+    var userLang = navigator.language || navigator.userLanguage; 
+
+    // 언어 코드를 기반으로 적절한 페이지 URL을 결정합니다.
+    var pageUrl = '';
+	if (userLang.includes('ja')) { // 일본어
+        pageUrl = 'index_ja.html';
+    }
+
+    // 페이지 URL이 설정되어 있으면 해당 페이지로 리디렉션합니다.
+    if (pageUrl) {
+        window.location.href = pageUrl;
+    }
+}
+
+// 페이지 로드 시 해당 함수를 실행합니다.
+redirectToLanguagePage();
